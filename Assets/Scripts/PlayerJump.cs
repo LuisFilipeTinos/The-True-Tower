f31d5d6a252d1ccb6884bae6ac45ed239662cc9f
Luis Filipe Tinos <lftinos@gmail.com>
2020-12-28 12:14:53 -0300

    The True Tower - CommitMovement
    Adição de elementos gráficos, implementação de mecânicas de movimentação.

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    GameObject groundCheck;
+
+    Rigidbody2D rb2d;
+
+    [SerializeField]
+    float jumpSpeed = 3f;
+
+    public bool isGrounded = true;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Physics2D.Linecast(transform.position, groundCheck.transform.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if(Input.GetKey("space") && isGrounded == true)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 2f;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if(Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+    }
+}
