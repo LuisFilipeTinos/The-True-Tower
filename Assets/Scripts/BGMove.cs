2192ebd01f7cfa1d408290975361d0dd169b8898
Luis Filipe Tinos <lftinos@gmail.com>
2020-12-28 10:41:15 -0300

    Projeto Game Jam

---
+++ Assets/Scripts/BGMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMove : MonoBehaviour
+{
+    public MeshRenderer renderer;
+    public float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset += new Vector2(0, speed * Time.deltaTime);
+    }
+}
